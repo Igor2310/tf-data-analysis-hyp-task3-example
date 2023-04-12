cf3de70583ff11bec0899af699a2a4457cbcc3e7
Igor2310 <80477307+Igor2310@users.noreply.github.com>
2023-04-13 00:04:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy import stats
+import statsmodels.stats.weightstats as w
+
 
 chat_id = 1152225195 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool: 
-    return x.mean() - stats.norm.ppf(0.94)*x.std()/(np.sqrt(x.shape[0])) >= 500
+def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    _, pvalue = w.ztest(x, value=500, alternative='larger')
+    return pvalue <= 0.06
